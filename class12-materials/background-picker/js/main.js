f00e7c3d2c041d638a800a3099c10a3a944457aa
alabador <austinlabador@gmail.com>
2022-10-24 15:26:45 -0700

    First commit and add orange selector

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -1,6 +1,7 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
+document.getElementById('purple').onclick = partyPurple;
+document.getElementById('green').onclick = partyGreen;
+document.getElementById('blue').onclick = partyBlue;
+document.getElementById('orange').onclick = partyOrange;
 
 
 function partyPurple() {
@@ -16,4 +17,9 @@ function partyGreen() {
 function partyBlue() {
   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
   document.querySelector('body').style.color = 'white'
+}
+
+function partyOrange() {
+  document.querySelector('body').style.backgroundColor = 'rgb(250, 204, 51)';
+  document.querySelector('body').style.color = 'white';
 }
