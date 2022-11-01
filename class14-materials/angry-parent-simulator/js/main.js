21c81510a725a163cccc965bc5caff807e2a54f2
alabador <austinlabador@gmail.com>
2022-10-31 22:18:04 -0700

    Complete angry parent sim

--- class14-materials/angry-parent-simulator/js/main.js
+++ class14-materials/angry-parent-simulator/js/main.js
@@ -1,4 +1,5 @@
 document.querySelector('#yell').addEventListener('click', run)
+document.querySelector('#work').addEventListener('click', nice)
 
 function run() {
   const fName = document.querySelector('#firstName').value
@@ -6,27 +7,28 @@ function run() {
   const lMidName = document.querySelector('#lastMiddle').value
   const lName = document.querySelector('#lastName').value
 
-  // document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
-
-  //Add what you should be doing - conditionals go here
-
   document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
+}
+
+function nice() {
+  document.querySelector('#placeToYell').innerText = 'Good Job!';
+  document.querySelector('#placeToYell').style.color = 'green';
 }
 
 // const synth = window.speechSynthesis;
 // document.querySelector('#yell').addEventListener('click', run)
-//
+
 // function run() {
 //   const fName = document.querySelector('#firstName').value
 //   const fMidName = document.querySelector('#firstMiddle').value
 //   const lMidName = document.querySelector('#lastMiddle').value
 //   const lName = document.querySelector('#lastName').value
-//
+
 //   const yellText =  `${fName} ${fMidName} ${lMidName} ${lName}`
-//
+
 //   document.querySelector('#placeToYell').innerText = yellText
-//
+
 //   let yellThis = new SpeechSynthesisUtterance(yellText);
-//
+
 //   synth.speak(yellThis);
 // }
