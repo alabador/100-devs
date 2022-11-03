e4bb7eb7b4b7d4399beb1f273ecd90f1078b5851
alabador <austinlabador@gmail.com>
2022-11-03 16:46:13 -0700

    Complete temperature converter

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -4,4 +4,16 @@
 //function (temp)
 //convert to selected temp
 //return converted temp to user
+//change innertext to returned value
 
+const input = document.querySelector('#temp-input');
+const button = document.querySelector('#convert');
+const temp = document.querySelector('.temp');
+
+function convertTemp(temp){
+    return (temp - 32) * (5/9) + ' C';
+}
+
+button.addEventListener('click', function() {
+    temp.innerText = convertTemp(input.value);
+});
