b67053dfb95d3172b082fb2548ca383adc676e49
alabador <austinlabador@gmail.com>
2022-11-03 15:32:14 -0700

    Complete simple functions

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,18 @@
 //create a function that adds two numbers and alerts the sum
+function sum(a,b) {
+    alert(a+b);
+}
 
 //create a function that multiplys three numbers and console logs the product
 
+function multiplyThreeNums(a,b,c) {
+    console.log(a*b*c);
+}
+
 //create a function that divides two numbers and returns the ???
+function divide(a,b){
+    if (b === 0) return "Cannot divide by 0";
+    else {
+        return a/b;
+    }
+}
